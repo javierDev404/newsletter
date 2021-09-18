35e89cb289f94d43774c510bf33ca716c8aea48b
thedevjay <thedevjay@gmail.com>
2021-09-17 23:27:20 -0700

    fix file format

--- app.js
+++ app.js
@@ -39,11 +39,10 @@ app.post("/", function(req, res) {
 		auth: "javier:38c4dfca9cd94c0699b08e5809671d0f-us5"
 	}
 	const request = https.request(url, options, function(response) {
-		if (response.statusCode ===200){
-			res.sendFile(__dirname+"/success.html")
-		}
-		else{
-			res.sendFile(__dirname+"/failure.html")
+		if (response.statusCode === 200) {
+			res.sendFile(__dirname + "/success.html")
+		} else {
+			res.sendFile(__dirname + "/failure.html")
 		}
 		response.on("data", function(data) {
 			console.log(JSON.parse(data));
@@ -55,7 +54,7 @@ app.post("/", function(req, res) {
 	request.end();
 });
 
-app.post("/failure", function(req,res){
+app.post("/failure", function(req, res) {
 	res.redirect("/");
 });
 
